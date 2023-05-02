729d72b9059120786bf1a383d53dc6a92da25281
VikashKumarX <129828854+VikashKumarX@users.noreply.github.com>
2023-05-02 14:11:51 +0530

    Sample list operations version1

---
+++ ListLearn/Demosamplelist.py
@@ -0,0 +1,34 @@
+# Python program to demonstrate addition of elements in a List
+# Creating a List
+spam = [2, 4, 6, 8, 10]
+print("Initial List: ")
+print(spam)
+# Addition of Element at specific Position (using Insert Method)
+spam.insert(3, 'hello')
+print("\nList after performing Insert Operation: ")
+print(spam)
+
+# Sample Output
+'''
+Initial List: 
+[2, 4, 6, 8, 10]
+List after performing Insert Operation: 
+[2, 4, 6, 'hello', 8, 10]
+'''
+
+# python program to demonstrate working of append and insert function
+# assign list
+demovar = ['geeks','vs']
+# use method
+demovar.append('nerds')
+# display list
+print(demovar)
+# Sample output ['geeks' , 'vs' ,'nerds']
+
+# assign list
+demovas2 = ['material', 'solid']
+# use method
+demovas2.insert(1, 'is')
+# display list
+print(demovas2)
+# Sample output ['material' , 'is' ,'solid']
